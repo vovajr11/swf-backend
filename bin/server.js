66cf796cd81b10488af9103c016290b5f29d3e23
Vova Moskalyuk <vova.moskalyuk11jr@gmail.com>
2022-03-03 22:46:02 +0200

    test

--- bin/server.js
+++ bin/server.js
@@ -4,11 +4,9 @@ const app = require('../app');
 
 const { DB_HOST } = process.env;
 
-const port = process.env.PORT || 4000;
-
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(port))
+  .then(() => app.listen(process.env.PORT || 3000))
   .catch(error => {
     console.log(error.message);
     process.exit(1);
