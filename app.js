4ef59aa46ad085923645c5c2ff39e942bc1c92a6
Vova Moskalyuk <vova.moskalyuk11jr@gmail.com>
2022-03-04 11:19:57 +0200

    added domain (test)

--- app.js
+++ app.js
@@ -13,9 +13,20 @@ const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
+const { DOMAIN_NAME } = process.env;
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', DOMAIN_NAME);
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 
 // app.use('/api/products', productsRouter)
 app.use('/api/auth', authRouter);
