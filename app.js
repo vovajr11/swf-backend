3b3bf30e4e3b6acff5bbb67243fbbda44124f00e
Vova Moskalyuk <vova.moskalyuk11jr@gmail.com>
2022-03-24 11:56:33 +0200

    edited header

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', DOMAIN_NAME);
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE',
